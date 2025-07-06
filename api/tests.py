53f42cc42bc104fc20617d6bba502af3f91c1911
Endrius-Albert <endriusamnoronha@gmail.com>
2025-07-06 17:52:00 +0100

    Add basic TESTcase for HealthView endpoint

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+class HealthViewTest(APITestCase):
+    def test_health_view(self):
+        url = reverse('health')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, {"status": "ok"})
