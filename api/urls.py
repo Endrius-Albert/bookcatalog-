fd1f98dc849b7df6983ab539ca9814dce14859e5
Endrius-Albert <endriusamnoronha@gmail.com>
2025-07-08 18:33:41 +0100

    Fix api URLs to use re_path and view functions

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
-from .views import RootView, HealthView, BookView
+from django.urls import re_path
+from .views import book_view, health_view
+
+app_name = 'api'
 
 urlpatterns = [
-    path('', RootView.as_view(), name='root'), 
-    path('health/', HealthView.as_view(), name='health'),
-    path('books/', BookView.as_view(), name='books'),
+    re_path(r"^$", health_view, name='health'),
+    re_path(r"^books/", book_view, name='books'),
 ]
